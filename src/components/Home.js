d4403bf10e374ca7c953c96931b29a86c6cc3d8a
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2021-07-30 16:13:36 +0200

    Load More component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,12 @@ import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
+  const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } =
+    useHomeFetch();
+
+  console.log(state);
+
+  if (error) return <div>Something went wrong ...</div>;
 
   return (
     <>
@@ -47,7 +52,7 @@ const Home = () => {
       </Grid>
       {loading && <Spinner />}
       {state.page < state.total_pages && !loading && (
-        <Button text="Load More" />
+        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
       )}
     </>
   );
