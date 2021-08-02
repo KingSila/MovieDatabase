b84b29958fb29f87d976ab2553f8ae4e8d6986b0
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2021-08-02 18:39:01 +0200

    added the sessionStorage

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,5 +1,8 @@
 import { useState, useEffect, useRef, useLayoutEffect } from "react";
 
+//helper
+import { isPersistedState } from "../helpers";
+
 //API
 import API from "../API";
 
@@ -38,7 +41,19 @@ export const useHomeFetch = () => {
   };
 
   //Initial and Search
+
   useEffect(() => {
+    if (!searchTerm) {
+      const sessionState = isPersistedState("homeState");
+
+      if (sessionState) {
+        console.log("Grabbing from SessionStorage");
+        setState(sessionState);
+        return;
+      }
+    }
+
+    console.log("Grabbing from API");
     setState(initialState);
     fetchMovies(1, searchTerm);
   }, [searchTerm]);
@@ -50,6 +65,13 @@ export const useHomeFetch = () => {
     fetchMovies(state.page + 1, searchTerm);
     setIsLoadingMore(false);
   }, [isLoadingMore, searchTerm, state.page]);
+
+  //Write to sessionStorage
+  useEffect(() => {
+    if (!searchTerm) {
+      sessionStorage.setItem("homeState", JSON.stringify(state));
+    }
+  }, [searchTerm, state]);
 
   return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
